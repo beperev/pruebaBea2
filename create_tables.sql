510bcda47ca363d7cab665698a25bb5b9099fe9b
beperev <46447323+beperev@users.noreply.github.com>
2020-03-04 16:31:04 +0100

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,43 +1,11 @@
-# Create Testuser
--- CREATE USER 'prbd'@'localhost' IDENTIFIED BY 'prbdprbd';
--- GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'prbd'@'localhost';
 # Create DB
 CREATE DATABASE IF NOT EXISTS `inmobiliaria` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
 USE `inmobiliaria`;
 
-CREATE DATABASE  IF NOT EXISTS `inmobiliaria` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `inmobiliaria`;
-
-
--- Table structure for table `captacion`
---
-show variables like 'lower%';
-
-DROP TABLE IF EXISTS `captacion`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-
-/*!40101 SET character_set_client = latin1 */;
-
-CREATE TABLE `captacion` (
-  
-	`ID_FINCA` varchar(10) DEFAULT NULL,
-  
-	`ID_EMPLEADO` varchar(6) DEFAULT NULL,
-  
-	`FECHA_CAPTACION` datetime DEFAULT NULL
-
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
 --
 -- Table structure for table `cliente`
 --
-
-
+SET FOREIGN_KEY_CHECKS=0; 
 DROP TABLE IF EXISTS `cliente`;
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -48,11 +16,11 @@ CREATE TABLE `cliente` (
  
 	 `ID_CLIENTE` varchar(6) NOT NULL,
   
-	`NOMBRE_CLIENTE` varchar(30) DEFAULT NULL,
+	`NOMBRE_CLIENTE` varchar(30) NOT NULL,
   
-	`APELLIDOS_CLIENTE` varchar(60) DEFAULT NULL,
+	`APELLIDOS_CLIENTE` varchar(60) NOT NULL,
   
-	`TELEFONO` varchar(9) DEFAULT NULL,
+	`TELEFONO` varchar(9) NOT NULL,
   
 	`PREFERENCIA` varchar(10) DEFAULT NULL,
   
@@ -62,6 +30,35 @@ CREATE TABLE `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+--
+-- Table structure for table `sucursal`
+--
+
+
+
+DROP TABLE IF EXISTS `sucursal`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
+/*!40101 SET character_set_client = latin1 */;
+
+
+CREATE TABLE `sucursal` (
+  
+`ID_SUCURSAL` varchar(6) NOT NULL,
+  
+`DIRECCION` varchar(20) NOT NULL,
+  
+`CIUDAD` varchar(20) NOT NULL,
+  
+`CODIGO_POSTAL` varchar(5) DEFAULT NULL,
+  
+PRIMARY KEY (`ID_SUCURSAL`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -79,65 +76,24 @@ CREATE TABLE `empleado` (
   
 	`ID_EMPLEADO` varchar(6) NOT NULL,
   
-	`NOMBRE_EMPLEADO` varchar(30) DEFAULT NULL,
+	`NOMBRE_EMPLEADO` varchar(30) NOT NULL,
   
-	`APELLIDOS_EMPLEADO` varchar(60) DEFAULT NULL,
+	`APELLIDOS_EMPLEADO` varchar(60) NOT NULL,
   
 	`TRABAJO` varchar(30) DEFAULT NULL,
   
 	`SEXO` varchar(1) DEFAULT NULL,
   
-	`FECHA_NACIMIENTO` datetime DEFAULT NULL,
+	`FECHA_NACIMIENTO` date DEFAULT NULL,
   
 	`SALARIO` double DEFAULT NULL,
   
-	`SUCURSAL` varchar(6) DEFAULT NULL,
+	`SUCURSAL` varchar(6) NOT NULL,
   
-	PRIMARY KEY (`ID_EMPLEADO`)
+	PRIMARY KEY (`ID_EMPLEADO`),
+    
+    CONSTRAINT `FK_ES` FOREIGN KEY (`SUCURSAL`) REFERENCES `Sucursal` (`ID_SUCURSAL`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-
---
--- Table structure for table `finca`
---
-
-
-DROP TABLE IF EXISTS `finca`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-
-/*!40101 SET character_set_client = latin1 */;
-
-
-CREATE TABLE `finca` (
-  
-	`ID_FINCA` varchar(10) NOT NULL,
-  
-	`DIRECCION` varchar(30) DEFAULT NULL,
-  
-	`CIUDAD` varchar(30) DEFAULT NULL,
-  
-	`CODIGO_POSTAL` varchar(5) DEFAULT NULL,
-  
-	`TIPO` varchar(15) DEFAULT NULL,
-  
-	`HABITACIONES` tinyint(3) unsigned DEFAULT NULL,
-  
-	`BANIOS` tinyint(3) unsigned DEFAULT NULL,
-  
-	`CALEFACCION` varchar(40) DEFAULT NULL,
-  
-	`ASCENSOR` varchar(2) DEFAULT NULL,
-  
-	`ALQUILER` double DEFAULT NULL,
-  
-	`PROPIETARIO` varchar(6) DEFAULT NULL,
-  
-	PRIMARY KEY (`ID_FINCA`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
@@ -160,13 +116,13 @@ CREATE TABLE `propietario` (
   
 	`ID_PROPIETARIO` varchar(6) NOT NULL,
   
-	`NOMBRE_PROPIETARIO` varchar(30) DEFAULT NULL,
+	`NOMBRE_PROPIETARIO` varchar(30) NOT NULL,
  
-	`APELLIDOS_PROPIETARIO` varchar(60) DEFAULT NULL,
+	`APELLIDOS_PROPIETARIO` varchar(60) NOT NULL,
   
-	`DIRECCION` varchar(60) DEFAULT NULL,
+	`DIRECCION` varchar(60) NOT NULL,
   
-	`TELEFONO` varchar(9) DEFAULT NULL,
+	`TELEFONO` varchar(9) NOT NULL,
   
 	PRIMARY KEY (`ID_PROPIETARIO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -178,36 +134,47 @@ CREATE TABLE `propietario` (
 
 
 
-
 --
--- Table structure for table `sucursal`
+-- Table structure for table `finca`
 --
 
 
-
-DROP TABLE IF EXISTS `sucursal`;
+DROP TABLE IF EXISTS `finca`;
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 
 /*!40101 SET character_set_client = latin1 */;
 
 
-CREATE TABLE `sucursal` (
+CREATE TABLE `finca` (
   
-`ID_SUCURSAL` varchar(6) NOT NULL,
+	`ID_FINCA` varchar(10) NOT NULL,
   
-`DIRECCION` varchar(20) DEFAULT NULL,
+	`DIRECCION` varchar(30) NOT NULL,
   
-`CIUDAD` varchar(20) DEFAULT NULL,
+	`CIUDAD` varchar(30) NOT NULL,
   
-`CODIGO_POSTAL` varchar(5) DEFAULT NULL,
+	`CODIGO_POSTAL` varchar(5) DEFAULT NULL,
   
-PRIMARY KEY (`ID_SUCURSAL`)
+	`TIPO` varchar(15) DEFAULT NULL,
+  
+	`HABITACIONES` tinyint(3) unsigned DEFAULT NULL,
+  
+	`BANIOS` tinyint(3) unsigned DEFAULT NULL,
+  
+	`CALEFACCION` varchar(40) DEFAULT NULL,
+  
+	`ASCENSOR` varchar(2) DEFAULT NULL,
+  
+	`ALQUILER` double NULL,
+  
+	`PROPIETARIO` varchar(6) NOT NULL,
+  
+	PRIMARY KEY (`ID_FINCA`),
+    CONSTRAINT `FK_FP` FOREIGN KEY (`PROPIETARIO`) REFERENCES `Propietario` (`ID_PROPIETARIO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 
 
 
@@ -225,17 +192,51 @@ DROP TABLE IF EXISTS `visita`;
 
 CREATE TABLE `visita` (
   
-	`ID_CLIENTE` varchar(6) DEFAULT NULL,
+	`ID_CLIENTE` varchar(6) NOT NULL,
   
-	`ID_FINCA` varchar(10) DEFAULT NULL,
+	`ID_FINCA` varchar(10) NOT NULL,
   
-	`ID_EMPLEADO` varchar(6) DEFAULT NULL,
+	`ID_EMPLEADO` varchar(6) NOT NULL,
   
-	`FECHA_VISITA` datetime DEFAULT NULL,
+	`FECHA_VISITA` datetime NOT NULL,
     
-	`COMENTARIOS` varchar(250) DEFAULT NULL
+	`COMENTARIOS` varchar(250) DEFAULT NULL,
+     PRIMARY KEY (`ID_CLIENTE`, `ID_FINCA`,`ID_EMPLEADO`,`FECHA_VISITA`),
+     CONSTRAINT `FK_VC` FOREIGN KEY (`ID_CLIENTE`) REFERENCES `Cliente` (`ID_CLIENTE`),
+     CONSTRAINT `FK_VF` FOREIGN KEY (`ID_FINCA`) REFERENCES `Finca` (`ID_FINCA`),
+     CONSTRAINT `FK_VE` FOREIGN KEY (`ID_EMPLEADO`) REFERENCES `Empleado` (`ID_EMPLEADO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-select * from `visita`;
+
+
+--
+-- Table structure for table `captacion`
+--
+
+
+
+DROP TABLE IF EXISTS `captacion`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
+/*!40101 SET character_set_client = latin1 */;
+
+CREATE TABLE `captacion` (
+  
+	`ID_FINCA` varchar(10) NOT NULL,
+  
+	`ID_EMPLEADO` varchar(6) NOT NULL,
+  
+	`FECHA_CAPTACION` datetime NOT NULL,
+
+	 PRIMARY KEY (`ID_FINCA`, `ID_EMPLEADO`,`FECHA_CAPTACION`),
+     CONSTRAINT `FK_CAF` FOREIGN KEY (`ID_FINCA`) REFERENCES `Finca` (`ID_FINCA`),
+     CONSTRAINT `FK_CAE` FOREIGN KEY (`ID_EMPLEADO`) REFERENCES `Empleado` (`ID_EMPLEADO`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+ 
+SET FOREIGN_KEY_CHECKS=1;
 # Add Data
